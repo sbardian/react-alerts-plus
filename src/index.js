fdcddf6b557e8d6a54352fca65a00060244877ae
Brian Andrews <sbardian@gmail.com>
2018-12-06 13:31:40 -0700

    Add TODO about splitting Provider and AlertWrapper

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import AlertContext from './AlertContext';
 import Provider from './Provider';
 
 export const AlertWrapper = ({ children: WrappedComponent }) => (
+  /**
+   * TODO:  Should i split Provider off and export independantly?
+   *        Enabling users to wrap their own app in a provider
+   *        and then using AlertWrapper when/where needed?
+   */
   <Provider>
     <AlertContext.Consumer>
       {({ show, close }) => {
