92c9ebd65c0ec9f7481778f2da232b4a022f3fb9
Brian Andrews <sbardian@gmail.com>
2018-12-12 09:42:29 -0700

    Remove emotion

--- src/Alert.js
+++ src/Alert.js
@@ -1,34 +1,30 @@
-/** @jsx jsx */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { jsx } from '@emotion/core';
 import { Icon } from 'react-icons-kit';
 import { ic_close as closeIcon } from 'react-icons-kit/md/ic_close';
 import { dark, light } from './AlertTemplates';
 
-const Alert = ({
-  alert: { style, id, message, /* IconComponent, CloseComponent, */ theme },
-  close,
-}) => (
-  <div id={id} css={theme === 'dark' ? dark : light} style={{ ...style }}>
-    {message}
-    <Icon
-      size={20}
-      icon={closeIcon}
-      style={{ float: 'right' }}
-      onClick={() => close(id)}
-    />
-  </div>
-);
+const Alert = ({ alert: { style, id, message, theme }, close }) => {
+  const applyTheme = theme === 'dark' ? dark : light;
+  return (
+    <div id={id} style={{ ...applyTheme, display: 'flex', ...style }}>
+      <span style={{ marginRight: '20px' }}>{message}</span>
+      <Icon
+        size={20}
+        icon={closeIcon}
+        style={{ position: 'absolute', right: '20px' }}
+        onClick={() => close(id)}
+      />
+    </div>
+  );
+};
 
 Alert.propTypes = {
   alert: PropTypes.shape({
-    duration: PropTypes.number,
     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     message: PropTypes.string,
     position: PropTypes.string,
-    IconComponent: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-    CloseComponent: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+    theme: PropTypes.string,
   }).isRequired,
   close: PropTypes.func.isRequired,
 };
