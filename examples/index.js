0b46c0147505a14788825e447fc2ec8a7e21e120
Brian Andrews <sbardian@gmail.com>
2018-12-20 20:44:21 -0700

    Update CardAlert example for responsiveness

--- examples/index.js
+++ examples/index.js
@@ -206,7 +206,6 @@ class App extends React.Component {
                                   border: 1px solid lavenderblush;
                                   justify-content: center;
                                   padding: 20px;
-                                  width: 400px;
                                   margin: 15px;
                                   background-color: cadetblue;
                                   box-shadow: 1px 1px 8px 1px #666;
