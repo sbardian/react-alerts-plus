48bdbafa8a2ae7a7f9a340d306bebaaea601429c
Brian Andrews <sbardian@gmail.com>
2019-02-09 20:01:09 -0700

    Add mock alert data

---
+++ src/__mocks__/alert-mocks.js
@@ -0,0 +1,44 @@
+export default {
+  topLeft: {
+    message: 'Alert ! ! !',
+    offset: '60px',
+    duration: 100,
+    position: 'top left',
+    id: 'test-alert-top-left',
+  },
+  topCenter: {
+    message: 'Alert ! ! !',
+    offset: '60px',
+    duration: 100,
+    position: 'top center',
+    id: 'test-alert-top-center',
+  },
+  topRight: {
+    message: 'Alert ! ! !',
+    offset: '60px',
+    duration: 100,
+    position: 'top right',
+    id: 'test-alert-top-right',
+  },
+  bottomLeft: {
+    message: 'Alert ! ! !',
+    offset: '60px',
+    duration: 100,
+    position: 'bottom left',
+    id: 'test-alert-bottom-left',
+  },
+  bottomCenter: {
+    message: 'Alert ! ! !',
+    offset: '60px',
+    duration: 100,
+    position: 'bottom center',
+    id: 'test-alert-bottom-center',
+  },
+  bottomRight: {
+    message: 'Alert ! ! !',
+    offset: '60px',
+    duration: 100,
+    position: 'bottom right',
+    id: 'test-alert-bottom-right',
+  },
+};
