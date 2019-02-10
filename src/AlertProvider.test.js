db293b6e078dcb0b5f6fd39e760205711680acbb
Brian Andrews <sbardian@gmail.com>
2019-02-09 20:02:16 -0700

    Default alert tests

--- src/AlertProvider.test.js
+++ src/AlertProvider.test.js
@@ -1,17 +1,9 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
-import { render, fireEvent } from 'test-utils';
+import { cleanup, render, fireEvent } from 'test-utils';
 import 'jest-dom/extend-expect';
 import AlertWrapper from './AlertWrapper';
-
-const topLeft = {
-  style: {},
-  message: 'Alert ! ! !',
-  offset: '60px',
-  duration: 100,
-  position: 'top left',
-  id: 'test-alert',
-};
+import alertMocks from './__mocks__/alert-mocks';
 
 const MyButton = ({ onClick, name }) => {
   return (
@@ -21,14 +13,19 @@ const MyButton = ({ onClick, name }) => {
   );
 };
 
-describe('AlertProvider ', () => {
-  it('tests wrapper', () => {
+afterEach(cleanup);
+
+describe('Default Alerts:', () => {
+  it('top left', () => {
     const { getByText, getByTestId, queryByTestId } = render(
       <AlertWrapper>
         {({ show, close }) => (
           <div>
-            <MyButton onClick={() => show(topLeft)} name="top left" />
-            <button type="button" onClick={() => close('test-alert')}>
+            <MyButton
+              onClick={() => show(alertMocks.topLeft)}
+              name="top left"
+            />
+            <button type="button" onClick={() => close('test-alert-top-left')}>
               close
             </button>
           </div>
@@ -36,8 +33,125 @@ describe('AlertProvider ', () => {
       </AlertWrapper>,
     );
     fireEvent.click(getByText('top left'));
-    expect(getByTestId('alert-container-test')).toBeTruthy();
+    expect(getByTestId(alertMocks.topLeft.id)).toBeTruthy();
     fireEvent.click(getByText('close'));
-    expect(queryByTestId('alert-container-test')).toBeFalsy();
+    expect(queryByTestId(alertMocks.topLeft.id)).toBeFalsy();
+  });
+  it('top center', () => {
+    const { getByText, getByTestId, queryByTestId } = render(
+      <AlertWrapper>
+        {({ show, close }) => (
+          <div>
+            <MyButton
+              onClick={() => show(alertMocks.topCenter)}
+              name="top center"
+            />
+            <button
+              type="button"
+              onClick={() => close('test-alert-top-center')}
+            >
+              close
+            </button>
+          </div>
+        )}
+      </AlertWrapper>,
+    );
+    fireEvent.click(getByText('top center'));
+    expect(getByTestId(alertMocks.topCenter.id)).toBeTruthy();
+    fireEvent.click(getByText('close'));
+    expect(queryByTestId(alertMocks.topCenter.id)).toBeFalsy();
+  });
+  it('top right', () => {
+    const { getByText, getByTestId, queryByTestId } = render(
+      <AlertWrapper>
+        {({ show, close }) => (
+          <div>
+            <MyButton
+              onClick={() => show(alertMocks.topRight)}
+              name="top right"
+            />
+            <button type="button" onClick={() => close('test-alert-top-right')}>
+              close
+            </button>
+          </div>
+        )}
+      </AlertWrapper>,
+    );
+    fireEvent.click(getByText('top right'));
+    expect(getByTestId(alertMocks.topRight.id)).toBeTruthy();
+    fireEvent.click(getByText('close'));
+    expect(queryByTestId(alertMocks.topRight.id)).toBeFalsy();
+  });
+  it('bottom left', () => {
+    const { getByText, getByTestId, queryByTestId } = render(
+      <AlertWrapper>
+        {({ show, close }) => (
+          <div>
+            <MyButton
+              onClick={() => show(alertMocks.bottomLeft)}
+              name="bottom left"
+            />
+            <button
+              type="button"
+              onClick={() => close('test-alert-bottom-left')}
+            >
+              close
+            </button>
+          </div>
+        )}
+      </AlertWrapper>,
+    );
+    fireEvent.click(getByText('bottom left'));
+    expect(getByTestId(alertMocks.bottomLeft.id)).toBeTruthy();
+    fireEvent.click(getByText('close'));
+    expect(queryByTestId(alertMocks.bottomLeft.id)).toBeFalsy();
+  });
+  it('bottom center', () => {
+    const { getByText, getByTestId, queryByTestId } = render(
+      <AlertWrapper>
+        {({ show, close }) => (
+          <div>
+            <MyButton
+              onClick={() => show(alertMocks.bottomCenter)}
+              name="bottom center"
+            />
+            <button
+              type="button"
+              onClick={() => close('test-alert-bottom-center')}
+            >
+              close
+            </button>
+          </div>
+        )}
+      </AlertWrapper>,
+    );
+    fireEvent.click(getByText('bottom center'));
+    expect(getByTestId(alertMocks.bottomCenter.id)).toBeTruthy();
+    fireEvent.click(getByText('close'));
+    expect(queryByTestId(alertMocks.bottomCenter.id)).toBeFalsy();
+  });
+  it('bottom right', () => {
+    const { getByText, getByTestId, queryByTestId } = render(
+      <AlertWrapper>
+        {({ show, close }) => (
+          <div>
+            <MyButton
+              onClick={() => show(alertMocks.bottomRight)}
+              name="bottom right"
+            />
+            <button
+              type="button"
+              onClick={() => close('test-alert-bottom-right')}
+            >
+              close
+            </button>
+          </div>
+        )}
+      </AlertWrapper>,
+    );
+    fireEvent.click(getByText('bottom right'));
+    expect(getByTestId(alertMocks.bottomRight.id)).toBeTruthy();
+    fireEvent.click(getByText('close'));
+    expect(queryByTestId(alertMocks.bottomRight.id)).toBeFalsy();
   });
 });
