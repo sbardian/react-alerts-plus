04e50dcd335a9048dba1bac913ee8452112f28d8
Brian Andrews <sbardian@gmail.com>
2019-09-06 10:08:54 -0600

    ci(circleci): update circleci config for jest and cypress

--- cypress/e2e/alerts.js
+++ cypress/e2e/alerts.js
@@ -1,62 +1,60 @@
+beforeEach(() => {
+  cy.visit('http://localhost:3001/')
+})
+
 describe('Test all alert positions in example app', () => {
   it('top left', () => {
-    cy.visit('/')
-      .findByTestId('top left')
+    cy.findByTestId('top left')
       .click()
       .findByText('default message')
       .should('exist')
       .wait(5040)
       .queryByText('default message')
-      .should('not.exist');
-  });
+      .should('not.exist')
+  })
   it('top center', () => {
-    cy.visit('/')
-      .findByTestId('top center')
+    cy.findByTestId('top center')
       .click()
       .findByText('default message')
       .should('exist')
       .wait(5040)
       .queryByText('default message')
-      .should('not.exist');
-  });
+      .should('not.exist')
+  })
   it('top right', () => {
-    cy.visit('/')
-      .findByTestId('top right')
+    cy.findByTestId('top right')
       .click()
       .findByText('There was an error processing your request.')
       .should('exist')
       .wait(8040)
       .queryByText('There was an error processing your request.')
-      .should('not.exist');
-  });
+      .should('not.exist')
+  })
   it('bottom left', () => {
-    cy.visit('/')
-      .findByTestId('bottom left')
+    cy.findByTestId('bottom left')
       .click()
       .findByText('default message')
       .should('exist')
       .wait(5040)
       .queryByText('default message')
-      .should('not.exist');
-  });
+      .should('not.exist')
+  })
   it('bottom center', () => {
-    cy.visit('/')
-      .findByTestId('bottom left')
+    cy.findByTestId('bottom left')
       .click()
       .findByText('default message')
       .should('exist')
       .wait(5040)
       .queryByText('default message')
-      .should('not.exist');
-  });
+      .should('not.exist')
+  })
   it('bottom left', () => {
-    cy.visit('/')
-      .findByTestId('bottom left')
+    cy.findByTestId('bottom left')
       .click()
       .findByText('default message')
       .should('exist')
       .wait(5040)
       .queryByText('default message')
-      .should('not.exist');
-  });
-});
+      .should('not.exist')
+  })
+})
